16c35da08975af9ec6c406d971bee9de3378f081
jiangwenping <jwpttcg66>
2017-01-09 11:01:26 +0800

    增加reactor机制 eventbus

--- src/main/java/com/snowcattle/game/excutor/thread/UpdateThread.java
+++ src/main/java/com/snowcattle/game/excutor/thread/UpdateThread.java
@@ -9,13 +9,34 @@ import java.util.concurrent.locks.LockSupport;
  * Created by jiangwenping on 17/1/9.
  */
 public class UpdateThread implements Runnable{
-
+    /**
+     * 调度线程
+     */
     private DispatchThread dispatchThread;
+    /**
+     * 事件总线
+     */
     private EventBus eventBus;
+
+
     private IUpdate iUpdate;
+
+    public UpdateThread(DispatchThread dispatchThread, EventBus eventBus) {
+        this.dispatchThread = dispatchThread;
+        this.eventBus = eventBus;
+    }
 
     public void run() {
         iUpdate.update();
         LockSupport.unpark(dispatchThread);
+        iUpdate = null;
+    }
+
+    public IUpdate getiUpdate() {
+        return iUpdate;
+    }
+
+    public void setiUpdate(IUpdate iUpdate) {
+        this.iUpdate = iUpdate;
     }
 }
