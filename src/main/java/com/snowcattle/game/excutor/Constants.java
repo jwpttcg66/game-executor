073c91ceb350a23db442e4806fcbb7c9a1277874
jiangwenping <jwpttcg66>
2017-01-09 18:24:52 +0800

    init

--- src/main/java/com/snowcattle/game/excutor/Constants.java
+++ src/main/java/com/snowcattle/game/excutor/Constants.java
@@ -8,8 +8,12 @@ import com.snowcattle.game.excutor.event.EventTypeEnum;
  */
 public class Constants {
 
+    /**
+     * ⌚事件类型常量
+     */
     public static class EventTypeConstans{
         public static EventType createEventType = new EventType(EventTypeEnum.create.ordinal());
-
+        public static EventType updateEventType = new EventType(EventTypeEnum.update.ordinal());
+        public static EventType finishEventType = new EventType(EventTypeEnum.finish.ordinal());
     }
 }
