35ae1ebd8e99cab4b34ebd0d2ec0a21b0c937003
jiangwenping <jwpttcg66>
2017-01-16 14:20:43 +0800

    增加准备监听事件

--- src/main/java/com/snowcattle/game/excutor/thread/LockSupportDisptachThread.java
+++ src/main/java/com/snowcattle/game/excutor/thread/LockSupportDisptachThread.java
@@ -11,12 +11,17 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class LockSupportDisptachThread extends DispatchThread{
 
+    private EventBus updateServiceEventBus;
+    private int cycleSize;
+
+    private boolean runningFlag = true;
+
     public LockSupportDisptachThread(EventBus eventBus) {
         super(eventBus);
     }
     public void run() {
         int maxSize = 10000;
-        while (true) {
+        while (runningFlag) {
             getEventBus().cycle(maxSize);
             LockSupport.park();
         }
@@ -29,5 +34,29 @@ public class LockSupportDisptachThread extends DispatchThread{
 
     public void unpark(){
         LockSupport.unpark(this);
+    }
+
+    public EventBus getUpdateServiceEventBus() {
+        return updateServiceEventBus;
+    }
+
+    public void setUpdateServiceEventBus(EventBus updateServiceEventBus) {
+        this.updateServiceEventBus = updateServiceEventBus;
+    }
+
+    public int getCycleSize() {
+        return cycleSize;
+    }
+
+    public void setCycleSize(int cycleSize) {
+        this.cycleSize = cycleSize;
+    }
+
+    public boolean isRunningFlag() {
+        return runningFlag;
+    }
+
+    public void setRunningFlag(boolean runningFlag) {
+        this.runningFlag = runningFlag;
     }
 }
