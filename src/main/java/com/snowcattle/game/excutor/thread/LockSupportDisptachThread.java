aad31df82c4f600aa280f1537d15bb94dc26eb7b
jiangwenping <jwpttcg66>
2017-01-16 15:49:11 +0800

    增加准备监听事件

--- src/main/java/com/snowcattle/game/excutor/thread/LockSupportDisptachThread.java
+++ src/main/java/com/snowcattle/game/excutor/thread/LockSupportDisptachThread.java
@@ -2,12 +2,15 @@ package com.snowcattle.game.excutor.thread;
 
 import com.snowcattle.game.excutor.event.Event;
 import com.snowcattle.game.excutor.event.EventBus;
+import com.snowcattle.game.excutor.pool.UpdateExecutorService;
 
 import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by jiangwenping on 17/1/9.
  * 带预置锁的分配器
+ *  接受create, update, finish事件
+ *   负责整个调度器的调度
  */
 public class LockSupportDisptachThread extends DispatchThread{
 
@@ -15,14 +18,18 @@ public class LockSupportDisptachThread extends DispatchThread{
     private int cycleSize;
 
     private boolean runningFlag = true;
+    private UpdateExecutorService updateExecutorService;
 
-    public LockSupportDisptachThread(EventBus eventBus) {
+    public LockSupportDisptachThread(EventBus eventBus, EventBus updateServiceEventBus, UpdateExecutorService updateExecutorService, int cycleSize) {
         super(eventBus);
+        this.updateServiceEventBus = updateServiceEventBus;
+        this.updateExecutorService = updateExecutorService;
+        this.cycleSize = cycleSize;
     }
+
     public void run() {
-        int maxSize = 10000;
         while (runningFlag) {
-            getEventBus().cycle(maxSize);
+            getEventBus().cycle(cycleSize);
             LockSupport.park();
         }
     }
@@ -58,5 +65,13 @@ public class LockSupportDisptachThread extends DispatchThread{
 
     public void setRunningFlag(boolean runningFlag) {
         this.runningFlag = runningFlag;
+    }
+
+    public UpdateExecutorService getUpdateExecutorService() {
+        return updateExecutorService;
+    }
+
+    public void setUpdateExecutorService(UpdateExecutorService updateExecutorService) {
+        this.updateExecutorService = updateExecutorService;
     }
 }
