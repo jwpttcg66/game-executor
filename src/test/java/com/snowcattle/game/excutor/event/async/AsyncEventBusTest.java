c2e72f5465dbd47ae09f2b3fbd7fa461a2a32377
jiangwenping <jwpttcg66>
2017-01-16 15:40:39 +0800

    增加准备监听事件

--- src/test/java/com/snowcattle/game/excutor/event/async/AsyncEventBusTest.java
+++ src/test/java/com/snowcattle/game/excutor/event/async/AsyncEventBusTest.java
@@ -5,10 +5,10 @@ package com.snowcattle.game.excutor.event.async;
  */
 
 import com.snowcattle.game.excutor.event.EventBus;
-import com.snowcattle.game.excutor.event.EventParam;
-import com.snowcattle.game.excutor.event.impl.*;
+import com.snowcattle.game.excutor.event.impl.DispatchCreateEventListener;
+import com.snowcattle.game.excutor.event.impl.ReadyCreateEventListener;
+import com.snowcattle.game.excutor.event.impl.ReadyFinishEventListener;
 import com.snowcattle.game.excutor.thread.LockSupportDisptachThread;
-import com.snowcattle.game.excutor.utils.Constants;
 
 /**
  * Created by jiangwenping on 17/1/9.
@@ -32,11 +32,11 @@ public class AsyncEventBusTest {
         //测试10万就够了
         long maxSize = 100000;
         dispatchThread.start();
-        for(long i = 0; i < maxSize; i++) {
-            EventParam<Integer> intParam = new EventParam<Integer>((int) i);
-            EventParam<Float> floatEventParam = new EventParam<Float>((float)(i+1));
-            TestCreateEvent event = new TestCreateEvent(Constants.EventTypeConstans.finishEventType, intParam, floatEventParam);
-            dispatchThread.addUpdateEvent(event);
-        }
+//        for(long i = 0; i < maxSize; i++) {
+//            EventParam<Integer> intParam = new EventParam<Integer>((int) i);
+//            EventParam<Float> floatEventParam = new EventParam<Float>((float)(i+1));
+//            TestCreateEvent event = new TestCreateEvent(Constants.EventTypeConstans.finishEventType, intParam, floatEventParam);
+//            dispatchThread.addUpdateEvent(event);
+//        }
     }
 }
