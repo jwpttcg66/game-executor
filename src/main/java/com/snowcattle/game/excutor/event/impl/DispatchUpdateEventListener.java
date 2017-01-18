be1153ab2af1f9c4009b6e9404a2350ce24cb160
jiangwenping <jwpttcg66>
2017-01-18 16:55:29 +0800

    增加update-future

--- src/main/java/com/snowcattle/game/excutor/event/impl/DispatchUpdateEventListener.java
+++ src/main/java/com/snowcattle/game/excutor/event/impl/DispatchUpdateEventListener.java
@@ -1,6 +1,9 @@
 package com.snowcattle.game.excutor.event.impl;
 
+import com.snowcattle.game.excutor.event.IEvent;
 import com.snowcattle.game.excutor.thread.DispatchThread;
+
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by jiangwenping on 17/1/11.
@@ -11,5 +14,15 @@ public class DispatchUpdateEventListener extends UpdateEventListener {
     public DispatchUpdateEventListener(DispatchThread dispatchThread) {
         super();
         this.dispatchThread = dispatchThread;
+    }
+
+    public void fireEvent(IEvent event) {
+        super.fireEvent(event);
+//        UpdateEvent updateEvent = new UpdateEvent(Constants.EventTypeConstans.updateEventType, event.getParams());
+//        this.dispatchThread.addUpdateEvent(updateEvent);
+
+        //提交执行线程
+
+        LockSupport.unpark(dispatchThread);
     }
 }
