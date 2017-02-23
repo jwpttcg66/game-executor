ce4820628f84b9d933c6d1bae8da72aa827541be
jwp <jwpttcg66@126.com>
2017-02-23 18:05:00 +0800

    删除多余bus,简化使用, 增加子线程池

--- src/main/java/com/snowcattle/game/excutor/thread/SingleLockSupportUpdateThread.java
+++ src/main/java/com/snowcattle/game/excutor/thread/SingleLockSupportUpdateThread.java
@@ -6,6 +6,8 @@ import com.snowcattle.game.excutor.event.impl.UpdateEvent;
 import com.snowcattle.game.excutor.update.IUpdate;
 import com.snowcattle.game.excutor.utils.Constants;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.LockSupport;
@@ -13,45 +15,60 @@ import java.util.concurrent.locks.LockSupport;
 /**
  * Created by jwp on 2017/2/23.
  */
-public class SingleLockSupportUpdateThread extends LockSupportUpdateThread{
+public class SingleLockSupportUpdateThread extends LockSupportUpdateThread {
 
     private Queue<IUpdate> iUpdates;
     private Queue<IUpdate> fetchUpdates;
+    private List<IUpdate> updateFinishList;
     private boolean runningFlag;
+
     public SingleLockSupportUpdateThread(DispatchThread dispatchThread, EventBus eventBus) {
         super(dispatchThread, eventBus);
         iUpdates = new ConcurrentLinkedQueue<IUpdate>();
         fetchUpdates = new ConcurrentLinkedQueue<IUpdate>();
+        updateFinishList = new ArrayList<>(updateFinishList);
         runningFlag = true;
     }
 
+    @Override
     public void run() {
 
         do {
-            for (;;) {
-                fetchUpdates();
+            fetchUpdates();
+            for (; ; ) {
                 IUpdate excutorUpdate = fetchUpdates.poll();
-                if (excutorUpdate!= null) {
+                if (excutorUpdate != null) {
                     excutorUpdate.update();
-                    setiUpdate(null);
-
-                    //事件总线增加更新完成通知
-                    EventParam<IUpdate> params = new EventParam<IUpdate>(excutorUpdate);
-                    UpdateEvent event = new UpdateEvent(Constants.EventTypeConstans.updateEventType, params);
-                    getEventBus().addEvent(event);
-                    LockSupport.unpark(getDispatchThread());
+                    updateFinishList.add(excutorUpdate);
+                } else {
+                    break;
                 }
 
             }
-
             //执行结束，进入等待状态，等待唤醒
-
-        }while (runningFlag);
+            try {
+                sendFinish();
+                wait();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } while (runningFlag);
 
     }
 
-    public void fetchUpdates(){
+    public void fetchUpdates() {
         fetchUpdates.addAll(iUpdates);
+    }
+
+    public void sendFinish() {
+        for (IUpdate excutorUpdate : updateFinishList) {
+            //事件总线增加更新完成通知
+            EventParam<IUpdate> params = new EventParam<IUpdate>(excutorUpdate);
+            UpdateEvent event = new UpdateEvent(Constants.EventTypeConstans.updateEventType, params);
+            getEventBus().addEvent(event);
+        }
+        updateFinishList.clear();
+        LockSupport.unpark(getDispatchThread());
     }
 
 
