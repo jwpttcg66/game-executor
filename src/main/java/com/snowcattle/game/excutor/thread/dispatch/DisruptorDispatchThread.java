620d2f18c8abc5b3a958e11b37f464fe7de64db4
jiangwenping <jwpttcg66>
2017-04-24 14:39:48 +0800

    增加disruptor

--- src/main/java/com/snowcattle/game/excutor/thread/dispatch/DisruptorDispatchThread.java
+++ src/main/java/com/snowcattle/game/excutor/thread/dispatch/DisruptorDispatchThread.java
@@ -4,5 +4,5 @@ package com.snowcattle.game.excutor.thread.dispatch;
  * Created by jiangwenping on 17/4/24.
  *
  */
-public class DisruptorDispatchThrad {
+public class DisruptorDispatchThread {
 }
