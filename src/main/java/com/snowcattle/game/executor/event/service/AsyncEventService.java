61deb54b4c574e5f7e395b24ef741b3a50c24ad2
jiangwenping <jwpttcg66>
2017-05-04 12:31:48 +0800

    异步线程服务

--- src/main/java/com/snowcattle/game/executor/event/service/AsyncEventService.java
+++ src/main/java/com/snowcattle/game/executor/event/service/AsyncEventService.java
@@ -2,7 +2,7 @@ package com.snowcattle.game.executor.event.service;
 
 import com.snowcattle.game.executor.event.EventBus;
 import com.snowcattle.game.executor.event.common.IEvent;
-import com.snowcattle.game.thread.executor.NonOrderedQueuePoolExecutor;
+import com.snowcattle.game.thread.executor.OrderedQueuePoolExecutor;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -17,11 +17,20 @@ public class AsyncEventService {
 
     private BlockingQueue<IEvent> queue;
 
-    private NonOrderedQueuePoolExecutor orderedQueuePoolExecutor;
-    public AsyncEventService(EventBus eventBus, int queueSize, int workSize, String threadFactoryName) {
+    private OrderedQueuePoolExecutor orderedQueuePoolExecutor;
+
+    /**
+     *
+     * @param eventBus
+     * @param queueSize 生产者队列大小
+     * @param workSize  消费者工作线程
+     * @param threadFactoryName 执行线程池名字
+     * @param orderQueueMaxSize  顺序执行线程池队列大小
+     */
+    public AsyncEventService(EventBus eventBus, int queueSize, int workSize, String threadFactoryName, int orderQueueMaxSize) {
         this.eventBus = eventBus;
         queue = new ArrayBlockingQueue<IEvent>(queueSize);
-        orderedQueuePoolExecutor = new NonOrderedQueuePoolExecutor(threadFactoryName, workSize);
+        orderedQueuePoolExecutor = new OrderedQueuePoolExecutor(threadFactoryName, workSize, orderQueueMaxSize);
     }
 
     public void startUp(){
